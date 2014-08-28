69a43a29ea35618b9edec56d28a353ad2ae75422
nobuya <nobuya@cs.okayama-u.ac.jp>
2014-08-28 14:44:18 +0900

    added popup window

--- app/src/main/java/com/example/nobuya/helloandroid0/MainActivity.java
+++ app/src/main/java/com/example/nobuya/helloandroid0/MainActivity.java
@@ -8,9 +8,16 @@ import android.view.View;
 import android.content.Intent;
 import android.widget.Toast;
 import android.widget.Button;
+import android.widget.PopupWindow;
+import android.util.TypedValue;
+import android.view.WindowManager;
+import android.view.Gravity;
 
 
 public class MainActivity extends Activity {
+
+    private PopupWindow popupWindow;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,6 +39,7 @@ public class MainActivity extends Activity {
                 Toast.makeText(MainActivity.this, "clicked!!", Toast.LENGTH_LONG).show();
             }
         });
+
     }
 
 
@@ -50,9 +58,49 @@ public class MainActivity extends Activity {
         int id = item.getItemId();
         if (id == R.id.action_settings) {
             return true;
+        } else if (id == R.id.action_about) {
+            Toast.makeText(MainActivity.this, "About...", Toast.LENGTH_LONG).show();
+            handleAboutMenu();
+            return true;
         }
+
         return super.onOptionsItemSelected(item);
     }
 
+
+    private void handleAboutMenu() {
+        popupWindow = new PopupWindow(MainActivity.this);
+        View popupView = getLayoutInflater().inflate(R.layout.popup_layout, null);
+        popupView.findViewById(R.id.close_button).setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               if (popupWindow.isShowing())
+                   popupWindow.dismiss();
+           }
+        });
+        popupWindow.setContentView(popupView);
+        // background
+        popupWindow.setBackgroundDrawable(getResources().getDrawable(R.drawable.popup_background));
+
+        popupWindow.setOutsideTouchable(true);
+        popupWindow.setFocusable(true);
+
+        // size (width: 200dp)
+        float width = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 200, getResources().getDisplayMetrics());
+        popupWindow.setWindowLayoutMode((int) width, WindowManager.LayoutParams.WRAP_CONTENT);
+        popupWindow.setWidth((int) width);
+        popupWindow.setHeight(WindowManager.LayoutParams.WRAP_CONTENT);
+
+        // centering
+        popupWindow.showAtLocation(findViewById(R.id.textView), Gravity.CENTER, 0, 0);
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (popupWindow != null && popupWindow.isShowing()) {
+            popupWindow.dismiss();
+        }
+        super.onDestroy();
+    }
 
 }
